6adbaba1e15ce4b24e662e15a20a7fe0f081b8db
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-07-11 16:52:40 -0300

    test: create product - req01

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Camisa",
+        "Approve",
+        "22/03/2021",
+        "22/03/2027",
+        154789,
+        "Manter em local arejado"
+    )
+
+    assert product.id == 1
+    assert product.nome_do_produto == "Camisa"
+    assert product.nome_da_empresa == "Approve"
+    assert product.data_de_fabricacao == "22/03/2021"
+    assert product.data_de_validade == "22/03/2027"
+    assert product.numero_de_serie == 154789
+    assert product.instrucoes_de_armazenamento == "Manter em local arejado"

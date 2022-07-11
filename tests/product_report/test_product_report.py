30f75ea170d3a99e19633a27321f4400cb6fbb2c
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-07-11 17:09:45 -0300

    test: __repr__ method product - req02

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Camisa",
+        "Approve",
+        "22/03/2021",
+        "22/03/2027",
+        154789,
+        "Manter em local arejado"
+    )
+
+    assert str(product) == (
+        "O produto Camisa fabricado em 22/03/2021 "
+        "por Approve com validade até "
+        "22/03/2027 precisa ser armazenado "
+        "Manter em local arejado."
+        )

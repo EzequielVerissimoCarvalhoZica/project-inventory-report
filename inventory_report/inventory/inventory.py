6d5644501bf56ec9887baeb0746c22dcfa6ab212
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-07-12 15:47:26 -0300

    feat: read json req 06

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -15,15 +16,26 @@ class Inventory:
         return report
 
     @classmethod
-    def valid_csv_file(cls, path):
+    def read_json(cls, path):
+        report = list()
+        with open(path, 'r') as file:
+            reader = json.load(file)
+            for row in reader:
+                report.append(row)
+        return report
+
+    @classmethod
+    def handle_file(cls, path):
         if path.endswith(".csv"):
             return Inventory.read_csv(path)
+        elif path.endswith('.json'):
+            return Inventory.read_json(path)
         else:
             raise ValueError("File type not supported")
 
     @classmethod
     def import_data(cls, path, report_type):
-        report = Inventory.valid_csv_file(path)
+        report = Inventory.handle_file(path)
 
         if report_type == "simples":
             return SimpleReport.generate(report)

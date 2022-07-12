328b8c1054620d7890f76f5bf96c32ebb4bd557c
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-07-12 15:39:32 -0300

    fix: req 05

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -5,18 +5,26 @@ from inventory_report.reports.simple_report import SimpleReport
 
 class Inventory:
     @classmethod
-    def import_data(cls, path, report_type):
-
-        report = []
-        if path.endswith(".csv"):
-            with open(path, mode="r") as file:
-                reader = csv.DictReader(file)
+    def read_csv(cls, path):
+        report = list()
+        with open(path, mode="r") as file:
+            reader = csv.DictReader(file)
             for row in reader:
                 report.append(row)
+
+        return report
+
+    @classmethod
+    def valid_csv_file(cls, path):
+        if path.endswith(".csv"):
+            return Inventory.read_csv(path)
         else:
             raise ValueError("File type not supported")
 
-        print(report, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    @classmethod
+    def import_data(cls, path, report_type):
+        report = Inventory.valid_csv_file(path)
+
         if report_type == "simples":
             return SimpleReport.generate(report)
         elif report_type == "completo":

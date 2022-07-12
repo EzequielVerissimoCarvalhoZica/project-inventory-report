bfab80567382cea4aa4f4c4474c4cba7f2462e62
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-07-12 15:59:55 -0300

    feat: xml file req 07 done

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xmltodict
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -18,9 +19,19 @@ class Inventory:
     @classmethod
     def read_json(cls, path):
         report = list()
-        with open(path, 'r') as file:
+        with open(path, mode="r") as file:
             reader = json.load(file)
             for row in reader:
+                report.append(row)
+        return report
+
+    @classmethod
+    def read_xml(cls, path):
+        report = list()
+        with open(path, mode="r") as file:
+            reader = xmltodict.parse(file.read())
+            format_reader = reader["dataset"]["record"]
+            for row in format_reader:
                 report.append(row)
         return report
 
@@ -28,8 +39,10 @@ class Inventory:
     def handle_file(cls, path):
         if path.endswith(".csv"):
             return Inventory.read_csv(path)
-        elif path.endswith('.json'):
+        elif path.endswith(".json"):
             return Inventory.read_json(path)
+        elif path.endswith(".xml"):
+            return Inventory.read_xml(path)
         else:
             raise ValueError("File type not supported")
 
